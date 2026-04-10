88495b1609882d72eea14a5bffc4c7de9ebbc035
unknown <LABORATORIO@SALAMAKER.LAB.ETEC187>
2026-04-09 21:28:46 -0300

    add campo de busca

--- app/src/main/java/com/aline/pokedex/MainActivity.kt
+++ app/src/main/java/com/aline/pokedex/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.aline.pokedex
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,11 +17,21 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
@@ -47,6 +58,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun StartScreen(modifier: Modifier = Modifier){
+    var nameOrId by remember { mutableStateOf("") }
+    Column(modifier = modifier
+        .fillMaxSize()
+        .background(Color(247, 247, 247, 255)),
+
+
+
+
+    ) {
     Column(modifier = Modifier
         .fillMaxWidth()
         .height(100.dp)
@@ -59,7 +79,7 @@ fun StartScreen(modifier: Modifier = Modifier){
     ) {
         Row(modifier = Modifier,)
         {
-            Spacer(modifier= modifier.width(10.dp))
+            Spacer(modifier= modifier.width(20.dp))
             Image(
             modifier = Modifier.size(40.dp),
             painter = painterResource(R.drawable.ic_pokebola),
@@ -67,12 +87,39 @@ fun StartScreen(modifier: Modifier = Modifier){
             contentDescription = "Pokebola"
         )
             Spacer(modifier= modifier.height(10.dp))
-            Spacer(modifier= modifier.width(10.dp))
+            Spacer(modifier= modifier.width(16.dp))
             Text(text = "Pokédex",
                 fontSize = (36.sp),
                 color = Color.White)}
 
+
+    }
+
+        OutlinedTextField(
+            value = nameOrId,
+            onValueChange = {nameOrId = it},
+            modifier = Modifier
+                .fillMaxWidth(.98f)
+                .padding(all = 20.dp)
+                .background(Color(247, 247, 247, 253)),
+            shape = RoundedCornerShape(20.dp),
+            
+
+
+            label = {Text(text = "Digite o nome ou o ID")},
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = " "
+
+                )
+            }
+        )
     }
 
 }
 
+@Composable
+fun CardPokemon(modifier: Modifier = Modifier) {
+    
+}
