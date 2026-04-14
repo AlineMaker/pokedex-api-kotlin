9d218d0ba45b9101075b98dd9191a77aebdec34b
unknown <LABORATORIO@SALAMAKER.LAB.ETEC187>
2026-04-14 17:13:39 -0300

    add cards

--- app/src/main/java/com/aline/pokedex/MainActivity.kt
+++ app/src/main/java/com/aline/pokedex/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.aline.pokedex
-
+import android.graphics.drawable.Drawable;
 import android.R.attr.height
 import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -21,7 +24,12 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonDefaults.buttonColors
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -35,6 +43,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.InspectableModifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,7 +59,6 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     StartScreen(modifier = Modifier.padding(innerPadding))
 
-
                 }
             }
         }
@@ -64,37 +72,34 @@ fun StartScreen(modifier: Modifier = Modifier){
         .fillMaxSize()
         .background(Color(247, 247, 247, 255)),
 
+        ) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .background(Color(199, 74, 64, 255)),
+
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Center
 
 
-
-    ) {
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .height(100.dp)
-        .background(Color(199, 74, 64, 255)),
-
-        horizontalAlignment = Alignment.Start,
-        verticalArrangement = Arrangement.Center
-
-
-    ) {
-        Row(modifier = Modifier,)
-        {
-            Spacer(modifier= modifier.width(20.dp))
-            Image(
-            modifier = Modifier.size(40.dp),
-            painter = painterResource(R.drawable.ic_pokebola),
-            colorFilter = ColorFilter.tint(color = Color.White),
-            contentDescription = "Pokebola"
-        )
-            Spacer(modifier= modifier.height(10.dp))
-            Spacer(modifier= modifier.width(16.dp))
-            Text(text = "Pokédex",
-                fontSize = (36.sp),
-                color = Color.White)}
+        ) {
+            Row(modifier = Modifier,)
+            {
+                Spacer(modifier= modifier.width(20.dp))
+                Image(
+                    modifier = Modifier.size(40.dp),
+                    painter = painterResource(R.drawable.ic_pokebola),
+                    colorFilter = ColorFilter.tint(color = Color.White),
+                    contentDescription = "Pokebola"
+                )
+                Spacer(modifier= modifier.height(10.dp))
+                Spacer(modifier= modifier.width(16.dp))
+                Text(text = "Pokédex",
+                    fontSize = (36.sp),
+                    color = Color.White)}
 
 
-    }
+        }
 
         OutlinedTextField(
             value = nameOrId,
@@ -104,7 +109,7 @@ fun StartScreen(modifier: Modifier = Modifier){
                 .padding(all = 20.dp)
                 .background(Color(247, 247, 247, 253)),
             shape = RoundedCornerShape(20.dp),
-            
+
 
 
             label = {Text(text = "Digite o nome ou o ID")},
@@ -114,20 +119,196 @@ fun StartScreen(modifier: Modifier = Modifier){
                     contentDescription = " "
 
                 )
+
             }
 
+
         )
+        CardPokemon()
     }
-    @Composable
-    fun CardPokemon(modifier: Modifier = Modifier,
-    ){
-        Column(modifier = Modifier
-            .height(30.dp)
-            .width(30.dp)
-            .background(Color(156, 39, 176, 255))) { }
-
-    }
-
-
 }
 
+
+@Composable
+fun CardPokemon(modifier: Modifier = Modifier){
+    Column(modifier = Modifier
+        .fillMaxSize(.98f)
+        .padding(all = 20.dp)
+        .background(Color(156, 39, 176, 255))
+    ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Card(
+                elevation = CardDefaults.cardElevation(8.dp),
+                shape = RoundedCornerShape(12.dp),
+                // colors = CardDefaults.cardColors(Color.Yellow)
+                border = BorderStroke(1.dp, Color.Yellow)
+            ){
+                Column(modifier = Modifier
+                    .width(115.dp)
+                    .height(130.dp)
+                    .background(
+                        color = Color(253, 255, 254, 255), shape = RoundedCornerShape(8.dp)
+                    )
+                    .border(1.dp, Color.Black, shape = RoundedCornerShape(12.dp)),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceBetween
+                ){
+                    Row(modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                        horizontalArrangement = Arrangement.End)
+                        { Text(text = "#001") }
+
+                    Image( modifier = Modifier.height(50.dp),
+                        painter = painterResource(R.drawable.ic_pokebola),
+                        colorFilter = ColorFilter.tint(color= Color.Yellow),
+                        contentDescription = "Pokemon"
+                    )
+
+
+                    OutlinedButton(
+                        onClick = {
+
+
+                        },
+
+                        modifier = Modifier
+                            .width(115.dp)
+                            .height(30.dp),
+
+
+                        shape = RoundedCornerShape( bottomStart = 8.dp, bottomEnd = 8.dp),
+                        colors = buttonColors(containerColor = Color.Yellow ),
+                        border = BorderStroke(1.dp, Color.Yellow)
+                    )
+                    {
+                        Text(
+                            text = "alternative",
+
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            color = Color.White
+                        )
+
+                    }
+                }
+            }
+
+            Spacer(modifier= modifier.width(9.dp))
+
+
+            Card(
+                elevation = CardDefaults.cardElevation(8.dp),
+                shape = RoundedCornerShape(12.dp),
+                // colors = CardDefaults.cardColors(Color.Yellow)
+
+            ){
+                Column(modifier = Modifier
+                    .width(115.dp)
+                    .height(130.dp)
+                    .background(
+                        color = Color(111, 215, 163), shape = RoundedCornerShape(8.dp)
+                    )
+                    .border(1.dp, Color.Black, shape = RoundedCornerShape(12.dp)),
+                    // verticalAlignment = Alignment.CenterVertically
+                ){
+
+                }
+            }
+
+            Spacer(modifier= modifier.width(9.dp))
+
+
+            Card(
+                elevation = CardDefaults.cardElevation(8.dp),
+                shape = RoundedCornerShape(12.dp),
+                // colors = CardDefaults.cardColors(Color.Yellow)
+
+            ){
+                Column(modifier = Modifier
+                    .width(115.dp)
+                    .height(130.dp)
+                    .background(
+                        color = Color(111, 215, 163), shape = RoundedCornerShape(8.dp)
+                    )
+                    .border(1.dp, Color.Black, shape = RoundedCornerShape(12.dp)),
+                    // verticalAlignment = Alignment.CenterVertically
+                ){
+
+                }
+            }
+
+
+
+        }
+
+        Spacer(modifier= modifier.height(9.dp))
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Card(
+                elevation = CardDefaults.cardElevation(8.dp),
+                shape = RoundedCornerShape(12.dp),
+                // colors = CardDefaults.cardColors(Color.Yellow)
+
+            ){
+                Column(modifier = Modifier
+                    .width(115.dp)
+                    .height(130.dp)
+                    .background(
+                        color = Color(111, 215, 163), shape = RoundedCornerShape(8.dp)
+                    )
+                    .border(1.dp, Color.Black, shape = RoundedCornerShape(12.dp)),
+                    // verticalAlignment = Alignment.CenterVertically
+                ){
+
+                }
+            }
+
+            Spacer(modifier= modifier.width(9.dp))
+
+
+            Card(
+                elevation = CardDefaults.cardElevation(8.dp),
+                shape = RoundedCornerShape(12.dp),
+                // colors = CardDefaults.cardColors(Color.Yellow)
+
+            ){
+                Column(modifier = Modifier
+                    .width(115.dp)
+                    .height(130.dp)
+                    .background(
+                        color = Color(255, 235, 59, 255), shape = RoundedCornerShape(8.dp)
+                    )
+                    .border(1.dp, Color.Black, shape = RoundedCornerShape(12.dp)),
+                    // verticalAlignment = Alignment.CenterVertically
+                ){
+
+                }
+            }
+
+            Spacer(modifier= modifier.width(9.dp))
+
+
+            Card(
+                elevation = CardDefaults.cardElevation(8.dp),
+                shape = RoundedCornerShape(12.dp),
+                // colors = CardDefaults.cardColors(Color.Yellow)
+
+            ){
+                Column(modifier = Modifier
+                    .width(115.dp)
+                    .height(130.dp)
+                    .background(
+                        color = Color(111, 215, 163), shape = RoundedCornerShape(8.dp)
+                    )
+                    .border(1.dp, Color.Black, shape = RoundedCornerShape(12.dp)),
+                    // verticalAlignment = Alignment.CenterVertically
+                ){
+
+                }
+            }
+        }
+
+
+    }
+
+}
